b232a34131d1e45d3550b44ee74751095385784f
Shane <shane8641@live.com>
2016-02-13 15:05:23 -0500

    added minimal css and leap year function

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-/**
- * Created by Shane on 2/13/2016.
- */
+function leapYear(){
+    var year = document.getElementById("year").value;
+    var leap = false;
+    if(year % 4 == 0){
+        if(year % 100 == 0){
+            if(year % 400 == 0){
+                leap = true;
+            }
+        }
+    }
+    if(leap == true){
+        window.alert("Leap year");
+    }
+    else{
+        window.alert("Not leap year")
+    }
+
+}
+document.getElementById("submit").addEventListener("click", leapYear, false);
